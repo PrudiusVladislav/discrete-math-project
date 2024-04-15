d07293716be760df58ffb1cf6ce04e07eeb324f4
Vlad Prudius <vladvlpr006@gmail.com>
2024-04-15 11:17:25 +0300

    add Edmonds-Karp max flow implementation

---
+++ DiscreteMath.Project.FordFulkerson/MaximumFlow.cs
@@ -0,0 +1,62 @@
+﻿namespace DiscreteMath.Project.FordFulkerson;
+
+public class MaximumFlow
+{
+    private readonly int[,] _capacity;
+    private readonly int[,] _flow;
+    private readonly int[] _parent;
+
+    public MaximumFlow(int[,] capacity)
+    {
+        _capacity = capacity;
+        int n = capacity.GetLength(0);
+        _flow = new int[n, n];
+        _parent = new int[n];
+    }
+
+    private bool BFS(int source, int sink)
+    {
+        bool[] visited = new bool[_capacity.GetLength(0)];
+        Queue<int> queue = new Queue<int>();
+        queue.Enqueue(source);
+        visited[source] = true;
+        _parent[source] = -1;
+
+        while (queue.Count > 0)
+        {
+            int u = queue.Dequeue();
+            for (int v = 0; v < _capacity.GetLength(0); v++)
+            {
+                if (!visited[v] && _capacity[u, v] - _flow[u, v] > 0)
+                {
+                    queue.Enqueue(v);
+                    _parent[v] = u;
+                    visited[v] = true;
+                }
+            }
+        }
+        return visited[sink];
+    }
+
+    public int FindMaxFlow(int source, int sink)
+    {
+        int maxFlow = 0;
+        while (BFS(source, sink))
+        {
+            int pathFlow = int.MaxValue;
+            for (int v = sink; v != source; v = _parent[v])
+            {
+                int u = _parent[v];
+                pathFlow = Math.Min(pathFlow, _capacity[u, v] - _flow[u, v]);
+            }
+            for (int v = sink; v != source; v = _parent[v])
+            {
+                int u = _parent[v];
+                _flow[u, v] += pathFlow;
+                _flow[v, u] -= pathFlow;
+            }
+            maxFlow += pathFlow;
+        }
+        return maxFlow;
+    }
+}
--- DiscreteMath.Project.FordFulkerson/Program.cs
+++ DiscreteMath.Project.FordFulkerson/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
-Console.WriteLine("Hello, World!");
+using DiscreteMath.Project.FordFulkerson;
+
+int[,] capacities = new int[,]
+{
+    {0, 10, 10, 0, 0, 0},
+    {0, 0, 0, 25, 0, 0},
+    {0, 0, 0, 0, 15, 0},
+    {0, 0, 0, 0, 0, 10},
+    {0, 6, 0, 0, 0, 10},
+    {0, 0, 0, 0, 0, 0}
+};
+
+int source = 0;
+int sink = 5;
+
+MaximumFlow maximumFlow = new(capacities);
+int maxFlow = maximumFlow.FindMaxFlow(source, sink);
+
+Console.WriteLine($"The maximum possible flow is {maxFlow}");
